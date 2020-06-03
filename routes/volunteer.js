1ee857bab02fab6be98cbeefa990da27c0625284
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-06-02 21:23:33 -0400

    added the passport functionality to the server.js file

--- routes/volunteer.js
+++ routes/volunteer.js
@@ -1,41 +1,44 @@
 var db = require("../models");
 
 module.exports = function (app) {
-    app.get("/api/volunteer", function (req, res) {
+  app.get("/api/volunteer", function (req, res) {
+    db.volunteer
+      .findAll({
+        include: [db.app],
+      })
+      .then(function (dbvolunteer) {
+        res.json(dbvolunteer);
+      });
+  });
 
-        db.volunteer.findAll({
-            include: [db.app]
-        }).then(function (dbvolunteer) {
-            res.json(dbvolunteer);
-        });
+  app.get("/api/volunteer/:id", function (req, res) {
+    db.volunteer
+      .findOne({
+        where: {
+          id: req.params.id,
+        },
+        include: [db.app],
+      })
+      .then(function (dbvolunteer) {
+        res.json(dbvolunteer);
+      });
+  });
+
+  app.post("/api/volunteer", function (req, res) {
+    db.volunteer.create(req.body).then(function (dbvolunteer) {
+      res.json(dbvolunteer);
     });
+  });
 
-    app.get("/api/volunteer/:id", function (req, res) {
-
-        db.volunteer.findOne({
-            where: {
-                id: req.params.id
-            },
-            include: [db.app]
-        }).then(function (dbvolunteer) {
-            res.json(dbvolunteer);
-        });
-    });
-
-    app.post("/api/volunteer", function (req, res) {
-        db.volunteer.create(req.body).then(function (dbvolunteer) {
-            res.json(dbvolunteer);
-        });
-    });
-
-    app.delete("/api/volunteer/:id", function (req, res) {
-        db.volunteer.destroy({
-            where: {
-                id: req.params.id
-            }
-        }).then(function (dbvolunteer) {
-            res.json(dbvolunteer);
-        });
-    });
-
+  app.delete("/api/volunteer/:id", function (req, res) {
+    db.volunteer
+      .destroy({
+        where: {
+          id: req.params.id,
+        },
+      })
+      .then(function (dbvolunteer) {
+        res.json(dbvolunteer);
+      });
+  });
 };
