82a57f288ba6b0b2382125cbcd99b6c20c9dd7e1
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-06-08 12:27:39 -0400

    updated html routes and started on deploying of the app to heroku

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,18 +4,24 @@ let isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function (app) {
   app.get("/", function (req, res) {
+    res.render("signup");
+  });
+  app.get("/login", function (req, res) {
     res.render("login");
   });
-
-  app.get("/login", function (req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/index");
-    }
-    res.sendFile(path.join(__dirname, "login"));
+  app.get("/index", function (req, res) {
+    res.render("index");
   });
 
-  app.get("/index", isAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname, "index"));
-  });
+  // app.get("/login", function (req, res) {
+  // If the user already has an account send them to the members page
+  // if (req.user) {
+  // res.rener("/index");
+  // }
+  // res.sendFile(path.join(__dirname, "login"));
+  // });
+
+  // app.get("/index", isAuthenticated, function (req, res) {
+  // res.sendFile(path.join(__dirname, "index"));
+  // });
 };
