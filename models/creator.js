8ed9deb4b24f3bc72cf88699aaea15cb806b84b0
Brandon P <brandonj.peart1@gmail.com>
2020-06-09 13:10:49 -0400

    login and role updates

--- models/creator.js
+++ models/creator.js
@@ -1,6 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
     let creator = sequelize.define("creator", {
-        name: DataTypes.STRING
+        name: DataTypes.STRING,
+        role: {
+            Type: DataTypes.BOOLEAN,
+            defualtValue: false
+        }
     });
 
     creator.associate = (models) => {
