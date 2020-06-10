fda4f90742afdf208e4117e2b07c4c07e13ea9a3
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-06-10 09:37:32 -0400

    Added a form for the functionality of adding an opportunity

--- routes/coordinator-routes.js
+++ routes/coordinator-routes.js
@@ -1,41 +1,44 @@
 let db = require("../models");
 
 module.exports = (app) => {
-    app.get("/api/coordinator", (req, res) => {
+  app.get("/api/coordinator", (req, res) => {
+    db.coordinator
+      .findAll({
+        include: [db.opportunity],
+      })
+      .then((dbcoordinator) => {
+        res.json(dbcoordinator);
+      });
+  });
 
-        db.coordinator.findAll({
-            include: [db.opportunity]
-        }).then((dbcoordinator) => {
-            res.json(dbcoordinator);
-        });
+  app.get("/api/coordinator/:id", (req, res) => {
+    db.coordinator
+      .findOne({
+        where: {
+          id: req.params.id,
+        },
+        include: [db.opportunity],
+      })
+      .then((dbcoordinator) => {
+        res.json(dbcoordinator);
+      });
+  });
+
+  app.post("/api/coordinator", (req, res) => {
+    db.coordinator.create(req.body).then((dbcoordinator) => {
+      res.json(dbcoordinator);
     });
+  });
 
-    app.get("/api/coordinator/:id", (req, res) => {
-
-        db.coordinator.findOne({
-            where: {
-                id: req.params.id
-            },
-            include: [db.opportunity]
-        }).then((dbcoordinator) => {
-            res.json(dbcoordinator);
-        });
-    });
-
-    app.post("/api/coordinator", (req, res) => {
-        db.coordinator.create(req.body).then((dbcoordinator) => {
-            res.json(dbcoordinator);
-        });
-    });
-
-    app.delete("/api/coordinator/:id", (req, res) => {
-        db.coordinator.destroy({
-            where: {
-                id: req.params.id
-            }
-        }).then((dbcoordinator) => {
-            res.json(dbcoordinator);
-        });
-    });
-
+  app.delete("/api/coordinator/:id", (req, res) => {
+    db.coordinator
+      .destroy({
+        where: {
+          id: req.params.id,
+        },
+      })
+      .then((dbcoordinator) => {
+        res.json(dbcoordinator);
+      });
+  });
 };
--- seeds.sql
+++ seeds.sql
@@ -1,35 +1,35 @@
-INSERT INTO volunteer.coordinators
-    (id, name, createdAt, updatedAt)
+INSERT INTO volunteer.coordinator
+    (id, email, createdAt, updatedAt)
 VALUES
-    (1, 'Soup Kitchen', 0 , 0 );
-INSERT INTO volunteer.coordinators
-    (id, name, createdAt, updatedAt)
+    (1, "soupKitchen@gmail.com", 0 , 0 );
+INSERT INTO volunteer.coordinator
+    (id, email, createdAt, updatedAt)
 VALUES
-    (2, 'Litter Cleanup', 0 , 0 );
+    (2, "highwayCleanup@gmail.com", 0 , 0 );
 
-INSERT INTO volunteer.volunteers
-    (id, volunteer_email, password,createdAt, updatedAt)
+INSERT INTO volunteer.volunteer
+    (id, email, password,createdAt, updatedAt)
 VALUES
     (1, "meow@gmail.com", "meow", 0 , 0 );
-INSERT INTO volunteer.volunteers
-    (id, volunteer_email, password, createdAt, updatedAt)
+INSERT INTO volunteer.volunteer
+    (id, email, password, createdAt, updatedAt)
 VALUES
     (2, "bark@gmail.com", "bark", 0 , 0 );
 
 
-INSERT INTO opportunities
-    (name, number, email, areaOfNeed, backgroundRequired, createdAt, updatedAt, creatorId, volunteerId)
+INSERT INTO volunteer.opportunities
+    (name, number, email, areaOfNeed, backgroundRequired, createdAt, updatedAt, coordinatorId, volunteerId)
 VALUES
-    ('Soup Kitchen', 5, "soupKitchen@gmail.com", "Line Cooks", true, 0 , 0 , 1 , 1  );
-INSERT INTO opportunities
-    (name, number, email, areaOfNeed, backgroundRequired, createdAt, updatedAt, creatorId, volunteerId)
+    ('Soup Kitchen', "254-545-5484", "soupKitchen@gmail.com", "Line Cooks", true, 0 , 0 , 1 , 1  );
+INSERT INTO volunteer.opportunities
+    (name, number, email, areaOfNeed, backgroundRequired, createdAt, updatedAt, coordinatorId, volunteerId)
 VALUES
-    ('Litter Cleanup', 6, "highwayCleanup@gmail.com", "Sunday Morning Help", false, 0 , 0 , 1 , 1  );
-INSERT INTO opportunities
-    (name, number, email, areaOfNeed, backgroundRequired, createdAt, updatedAt, creatorId, volunteerId)
+    ('Litter Cleanup', "267-548-5988", "highwayCleanup@gmail.com", "Sunday Morning Help", false, 0 , 0 , 1 , 1  );
+INSERT INTO volunteer.opportunities
+    (name, number, email, areaOfNeed, backgroundRequired, createdAt, updatedAt, coordinatorId, volunteerId)
 VALUES
-    ('Blood Drive', 6, "redCross@gmail.com", "Blood Drive Saturday", true, 0 , 0 , 1 , 1  );
-INSERT INTO opportunities
-    (name, number, email, areaOfNeed, backgroundRequired, createdAt, updatedAt, creatorId, volunteerId)
+    ('Blood Drive', "658-548-8575", "redCross@gmail.com", "Blood Drive Saturday", true, 0 , 0 , 1 , 1  );
+INSERT INTO volunteer.opportunities
+    (name, number, email, areaOfNeed, backgroundRequired, createdAt, updatedAt, coordinatorId, volunteerId)
 VALUES
-    ('Tutor Services', 6, "learningAssistant@gmail.com", "Math Tutor Help", true, 0 , 0 , 1 , 1  );
+    ('Tutor Services', "564-484-5845", "learningAssistant@gmail.com", "Math Tutor Help", true, 0 , 0 , 1 , 1  );
